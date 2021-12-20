b36ac5b9e67d1dcbfa5c43305878e0ac3df3f28c
Dahoon Kim <salqueng@gmail.com>
2021-12-20 11:45:49 +0900

    Minimal Crash Example

--- TCAiOS14CrashOnAlert/ContentView.swift
+++ TCAiOS14CrashOnAlert/ContentView.swift
@@ -6,16 +6,39 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
+
+enum AlertAction {
+  case dismiss
+}
+struct ContentState {
+  var alert: AlertState<AlertAction>?
+  init() {}
+}
+
+enum ContentAction {
+  case alert(_: AlertAction)
+}
+
+let contentReducer = Reducer<ContentState, ContentAction, Void>.init { _, _, _ in
+  return .none
+}
+
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+  let store: Store<ContentState, ContentAction>
+  init() {
+    store = .init(initialState: .init(), reducer: contentReducer, environment: ())
+  }
+  var body: some View {
+    Text("Hello, world!")
+      .padding()
+      .alert(store.scope(state: { $0.alert }, action: { .alert($0) }), dismiss: AlertAction.dismiss)
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
