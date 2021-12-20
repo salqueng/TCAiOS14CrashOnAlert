b7e307f65ebb80c4743a7d482e7efc115b01db39
Dahoon Kim <salqueng@gmail.com>
2021-12-20 10:53:40 +0900

    Initial Commit

---
+++ TCAiOS14CrashOnAlert/TCAiOS14CrashOnAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAiOS14CrashOnAlertApp.swift
+//  TCAiOS14CrashOnAlert
+//
+//  Created by Dahoon Kim on 2021/12/20.
+//
+
+import SwiftUI
+
+@main
+struct TCAiOS14CrashOnAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
